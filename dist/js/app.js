90a8a8f130fe2929436ca6968a40ac7ee76f401f
leonelnexman <reshevdev@gmail.com>
2024-08-17 09:20:38 +0300

    rockapp

--- dist/js/app.js
+++ dist/js/app.js
@@ -19,6 +19,34 @@ document.querySelector('.hamburger').addEventListener('click', function() {
     body.classList.toggle('overflow-hidden');
 });
 
+let SwiperTop = new Swiper('.swiper--top', {
+    spaceBetween: "0",
+    centeredSlides: true,
+    speed: 6000,
+    autoplay: {
+      delay: 1,
+    },
+    loop: true,
+    slidesPerView:'auto',
+    allowTouchMove: false,
+    disableOnInteraction: true
+  });
+  
+  let SwiperBottom = new Swiper('.swiper--bottom', {
+    spaceBetween: "0",
+    centeredSlides: true,
+    speed: 6000,
+    autoplay: {
+      delay: 1,
+      reverseDirection: true
+    },
+    loop: true,
+    loopedSlides: 4,
+    slidesPerView:'auto',
+    allowTouchMove: false,
+    disableOnInteraction: true
+  });
+
 
 var swiper = new Swiper('.swiper-container', {
     slidesPerView: 3,
